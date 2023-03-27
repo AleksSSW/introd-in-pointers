9c1ed3b1832221e64be929441e84d69b772fde0d
Александра <@LAPTOP-3S0DRT6C>
2023-03-27 21:58:27 +0500

    Добавьте файлы проекта.

---
+++ introd in pointers/introd in pointers.cpp
@@ -0,0 +1,21 @@
+﻿// introd in pointers.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    double x, y;
+    std::cout << "Введите занчения:\n";
+    std::cin >> x >> y;
+    double* px = &x;
+    double* py = &y;
+    double res = (*px + *py) / 2;
+    std::cout << "среднее арифметическое: - >" << res << '\n';
+        
+
+    return 0;
+}
+
+
